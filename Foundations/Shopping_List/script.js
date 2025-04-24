7614aafd44d69a5812dbc2486262ab38d32e8596
Ty700 <T.ScottiBusiness@yahoo.com>
2025-04-23 22:11:52 -0500

    Experiment with adding an "Enter" key functionality to adding list items.

--- Foundations/Shopping_List/script.js
+++ Foundations/Shopping_List/script.js
@@ -2,24 +2,43 @@ const list = document.querySelector("ul");
 const userInput = document.querySelector("input");
 const btn = document.querySelector("button");
 
-btn.addEventListener("click", () => {
+/*
+	* FUNCTION: Adds a new item to the 'list' object
+	* PARAMS: VOID
+	* RETURNS: VOID
+*/
+	
+function addNewItem() {
 	var userInputVal  = `${userInput.value}`;
+
+	if (userInputVal !== '') {
+		var listItem = document.createElement("li");
+		var listText = document.createElement("span");
+		var delete_button = document.createElement("button");
+
+		listText.textContent = userInputVal;
+		delete_button.textContent = "delete";
+
+		listItem.appendChild(listText);
+		listItem.appendChild(delete_button);
+		list.appendChild(listItem);
+
+		delete_button.addEventListener("click", () =>  {
+			list.removeChild(listItem);
+		});
+
+		userInput.focus();
+	}
 	userInput.value = '';
 
-	var listItem = document.createElement("li");
-	var listText = document.createElement("span");
-	var delete_button = document.createElement("button");
+}
 
-	listText.textContent = userInputVal;
-	delete_button.textContent = "delete";
+/* On user click of the btn, it will add an item */
+btn.addEventListener("click", addNewItem);
 
-	listItem.appendChild(listText);
-	listItem.appendChild(delete_button);
-	list.appendChild(listItem);
-
-	delete_button.addEventListener("click", () =>  {
-		list.removeChild(listItem);
-	});
-
-	userInput.focus();
+/* If the user clicks enter, it will add an item */
+userInput.addEventListener("keydown", function(e) {
+	if (e.key === 'Enter') {
+		addNewItem();
+	}
 });
